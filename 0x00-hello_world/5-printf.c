498f88e9d50082053a6f35c901181fcdabc5b11e
wbbxther <wunmiiyaomolere@gmail.com>
2022-06-30 16:02:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+
+/**
+ * main - Prints "with proper grammer, but the outcome is
+ * a piece of art,"followed by a new line.
+ *      
+ *      Return: Always 0.
+ */  
+int main(void)
+	 {
+		  prinf("with proper grammar, but the outcome is a piece of art,\n);
+		  
+return(0);
+	 }
