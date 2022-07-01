933c97ae4c4d1099c53c9dab9af90622725f9a2d
wbxther <wunmiiyaomolere@gmail.com>
2022-06-30 21:11:34 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - Prints "with proper grammer, but the outcome is
  * a piece of art,"followed by a new line.
  *
- *      Return: Always 0.
+ *      Return: Always 0. (Sucess)
  */
 int main(void)
 {
