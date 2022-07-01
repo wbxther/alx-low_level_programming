7c83dcebbc69e7cb5d6aaba5f12d16fb1a846ad0
wbxther <wunmiiyaomolere@gmail.com>
2022-06-30 21:50:28 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming isblike buliding a multilingual
+ * main - Prints "Programming is like building a multilingual
  * puzzle, followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like buliding a multilingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle ");
 
 return  (0);
 }
