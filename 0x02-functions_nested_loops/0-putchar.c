76841cc7c9d8b750c9120fccf1b370120b2c0f4f
wbxther <wunmiiyaomolere@gmail.com>
2022-07-05 16:34:20 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * Main - Entry
+ * Main - a program that prints _putchar 
+ * in new line
  *
  * Return: Always 0.
  */
@@ -10,7 +11,7 @@ int main(void)
 	char ch[] = "_putchar";
 	int i;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(ch[i]);
 	}
