117fc1743d741162ff39e9b23ecbad6a79b46b30
wbxther <wunmiiyaomolere@gmail.com>
2022-07-05 11:33:05 -0700

    task0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * _putchar - Write a program that prints _putchar
+ * Followed by new line
+ *
+ * Return: Always 0.
+ */
+int _putchar(char ch[] = "_putchar")
+{
+	putchar{'\n'}
+
+	return (0);
+}
