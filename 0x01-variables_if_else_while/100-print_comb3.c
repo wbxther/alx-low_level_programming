990b84f684fa79c37b0f5e7d8fc96ddf9562dac7
wbxther <wunmiiyaomolere@gmail.com>
2022-07-05 04:54:42 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,14 +17,14 @@ int main(void)
 		{
 			if (digit1 < digit2)
 			{
-			putchar((digit1);
-			putchar((digit2);
+			putchar((digit1));
+			putchar((digit2));
 
 			if (digit1 != 8 || (digit1 == 8 && digit2 != 9))
 			{
 			putchar(',');
 			putchar(' ');
-			}
+			};
 			}
 			}
 			}
