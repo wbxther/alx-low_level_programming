4dec0c0401a6785d062c748c1aaed4d0cdd08af1
wbxther <wunmiiyaomolere@gmail.com>
2022-07-05 14:18:05 -0700

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_
+#define MAIN_H
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +14,5 @@ void print_times_table(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void print_to_98(int n);
+
+#endif
