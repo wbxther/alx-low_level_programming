acfaae6a14582dcad22811ebefdcf98e867ca9bd
wbxther <wunmiiyaomolere@gmail.com>
2022-07-06 11:30:54 -0700

    task11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two numbets
+ * @x : integer 1
+ * @y :integer 2
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ * @n: the starting point
+ * Return: no return
+ */
+void print_to_98(int n)
+
+for (n == 98, n <= 98, n++)
+{
+	if (n == 98)
+		printf("%d\n", n);
+	else
+		printf("%d, ", n);
+	{
+		if (n == 98, n >= 98, n--)
+			printf("%d\n", n);
+		else
+			printf("%d, ", n);
+	}
+}
