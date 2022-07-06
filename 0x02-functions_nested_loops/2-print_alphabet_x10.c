f164e8afd0de418047d72464c6e81974e51b319a
wbxther <wunmiiyaomolere@gmail.com>
2022-07-05 19:29:13 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,13 @@ void print_alphabet_x10(void)
 	int i;
 	int k;
 
- for (k = 0; k <= 10; k++)
+	for (k = 0; k <= 9; k++)
 	{
-		for (i = 97; i <=122; i++)
+		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
 		}
-		
+
 		_putchar('\n');
 	}
 }
-
-
