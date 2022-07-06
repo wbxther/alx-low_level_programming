a2a72137a06086736e07fa301b0db365b37ea258
wbxther <wunmiiyaomolere@gmail.com>
2022-07-05 20:32:26 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * _isalpha - prints 1 if input is letter
+ *
+ * @c: character in ASCII code
+ *
+ * Return: 1 for letters. 0 otherwise
+ */
+int_isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n);
+}
