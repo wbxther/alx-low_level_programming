cd7b6932814a37f4201353fbced33965cbd65c14
wbxther <wunmiiyaomolere@gmail.com>
2022-07-05 19:12:31 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - Prints alphabets x10
+ *
+ * Result: void
+ */
+int i;
+int k;
+{
+	for (k = 0; k <= 10; k++)
+	{
+		for (i = 97; i <=122; i++)
+		{
+			_putchar(i);
+		}
+		
+		_putchar('\n');
+	}
+}
+
+
