a66f0c1de4a8a7e0b3f55136e4aede42af0795b2
wbxther <wunmiiyaomolere@gmail.com>
2022-07-06 17:21:05 -0700

    nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * print_times_table - prints the n times table, starting with 0
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,15 +18,16 @@ void print_to_98(int n)
 				printf("%d, ", n);
 			n++;
 		}
-	}else if (n > 98)
-				{
-					while (n >= 98)
-					{
-						if (n == 98)
-							printf("%d\n", n);
-						else
-							printf("%d, ",n);
-						n--;
-					}
-				}
-			}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+			n--;
+		}
+	}
+}
