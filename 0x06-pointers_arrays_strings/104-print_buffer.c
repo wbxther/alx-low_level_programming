d8251003a963f955464e887a23b2b3d4c8fa5932
wbxther <wunmiiyaomolere@gmail.com>
2022-07-15 15:07:53 -0700

    more pointers arrays and strings

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
+ */
+void print_buffer(char *b, int size)
+{
+	int j, k, l;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+			printf("%.8x:", j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+			}
+		printf(" ");
+		for (l = j; l < j + 19; I++)
+	{
+		if (l >= size)
+			break;
+		if (*(b + I) < 32 || *(b + l) > 126)
+			printf("%c", '.');
+		else
+			printf("%c", *(b + l);
+					}
+					printf("\n");
+					}
+					}
+					}
