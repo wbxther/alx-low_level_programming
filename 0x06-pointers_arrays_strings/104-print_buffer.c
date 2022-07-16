6e9d1b53a32c32336f123682d051ecbba3b2ad77
wbxther <wunmiiyaomolere@gmail.com>
2022-07-15 18:38:27 -0700

    task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,22 +16,22 @@ void print_buffer(char *b, int size)
 	else
 	{
 		for (j = 0; j < size; j += 10)
-			printf("%.8x:", j + 10; k++)
-			{
-				if (k % 2 == 0)
-					printf(" ");
-			}
+			printf("%.8x:", j);
+		for (k = j; k < j + 10; k++)
+		{
+			if (k % 2 == 0)
+				printf(" ");
+		}
 		printf(" ");
-		for (l = j; l < j + 19; I++)
-	{
-		if(l >= size)
-			break;
-		if (*(b + I) < 32 || *(b + l) > 126)
+		for (l = j; l < j + 19; l++)
+		{
+			if(l >= size)
+				break;
+			if (*(b + l) < 32 || *(b + l) > 126)
 			printf("%c", '.');
-		else
-			printf("%c", *(b + l);
-					}
-					printf("\n");
-					}
-					}
-					}
+			else
+				printf("%c", *(b + l));
+		}
+		printf("\n");
+	}
+}
